7de8732c9d7175fc87cffc44593f80b4d5d900ab
yo <palamp3105@gmail.com>
2026-03-12 20:18:47 +1300

    added proper observability script + grafana ingress

--- scripts/observability/install-stack.sh
+++ scripts/observability/install-stack.sh
@@ -5,10 +5,9 @@ helm repo update
 helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack \
   --namespace monitoring \
   --create-namespace \
-  --set grafana.grafana\.ini.server.root_url="http://%(domain)s/grafana" \
-  --set grafana.grafana\.ini.server.serve_from_sub_path=true \
-  --set grafana.adminPassword="testing1234!"
-
-kubectl rollout status deployment/kube-prometheus-stack-grafana -n monitoring
+  --set grafana.env.GF_SERVER_ROOT_URL="http://%(domain)s/grafana" \
+  --set grafana.env.GF_SERVER_SERVE_FROM_SUB_PATH="true" \
+  --set grafana.adminPassword="pala3105" \
+  --wait
 
 kubectl apply -f grafana-ingress.yaml -n monitoring
